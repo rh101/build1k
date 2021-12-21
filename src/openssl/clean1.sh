3f04b5e2ff313337ff14b2ab5a423d775b70d523
halx99 <halx99@live.com>
2021-12-21 18:12:13 +0800

    Fix build_ci

--- src/openssl/clean1.sh
+++ src/openssl/clean1.sh
@@ -5,11 +5,4 @@ if [ -d "$install_dir" ] ; then
     rm -rf $install_dir/bin
     rm -rf $install_dir/misc
     rm -rf $install_dir/share
-    if [ "$BUILD_TARGET" = "linux" ] ; then
-        mv $install_dir/lib64 $install_dir/lib
-    fi
-
-    if [ "$BUILD_TARGET" = "linux" ] || [ "$BUILD_TARGET" = "osx" ] ; then
-        rm -rf $install_dir/lib/ossl-modules
-    fi
 fi
